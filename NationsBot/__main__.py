f417bed9074e41b119e74cbd3eae9242b5e62379
AnuraagGodavari <anuraag.godavari@gmail.com>
2022-07-18 20:05:00 -0400

    First commit: basic bot framework

---
+++ NationsBot/__main__.py
@@ -0,0 +1,45 @@
+import os
+from dotenv import load_dotenv
+import json
+
+import discord
+from discord.ext import commands
+
+from common import *
+from database import *
+
+#The bot
+nationsbot = commands.Bot(command_prefix = 'n.', intents = discord.Intents().all())
+
+
+@nationsbot.event
+async def on_ready():
+	""" Detects when the bot has been fully loaded and is online """
+	print("Bot ready!")
+
+@nationsbot.command()
+async def load(ctx, cog):
+	pass
+
+@nationsbot.command()
+async def unload(ctx, cog):
+	pass
+
+		
+@nationsbot.command()
+async def ping(ctx):
+	await ctx.send(f"Pong!\nLatency: **{round(nationsbot.latency * 1000)}ms**")
+
+def main():
+	
+	load_dotenv()
+	token = os.getenv('TOKEN')
+	
+	for filename in os.listdir(f"{pwdir}/Cogs"):
+		if filename.endswith(".py"):
+			nationsbot.load_extension(f"Cogs.{filename[:-3]}")
+	
+	nationsbot.run(token)
+
+if __name__ == "__main__":
+	main()
---
+++ NationsBot/database.py
@@ -0,0 +1,30 @@
+import mysql.connector as mariadb, sys, os
+from dotenv import load_dotenv
+
+mariadb_connection = None
+
+def fetch_assoc(cursor):
+	return dict(zip(cursor.column_names, cursor.fetchone()))
+
+def getdb():
+	global mariadb_connection
+	
+	if not mariadb_connection:
+		
+		load_dotenv()
+		db_user = os.getenv('DB_USER')
+		db_pass = os.getenv('DB_PASS')
+		db_host = os.getenv('DB_HOST')
+		db_port = os.getenv('DB_PORT')
+		db_name = os.getenv('DB_DATABASE')
+
+		try:
+			connection = mariadb.connect(user=db_user, password=db_pass, host=db_host, port=db_port, database=db_name)
+			mariadb_connection = connection
+
+		except mariadb.Error as e:
+			print(f"Error connecting to MariaDB Platform: {e}")
+			return
+
+	return mariadb_connection
+
