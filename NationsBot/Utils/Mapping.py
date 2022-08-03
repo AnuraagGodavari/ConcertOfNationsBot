0ceed6216c8ea6168b211931ef77b3228d80e60c
AnuraagGodavari <anuraag.godavari@gmail.com>
2022-08-03 17:41:31 -0400

    Added automatic neighbor calculation for maps

--- NationsBot/Utils/Mapping.py
+++ NationsBot/Utils/Mapping.py
@@ -29,3 +29,38 @@ class World:
     def __init__(self, name, territories = None):
         self.name = name
         self.territories = territories or dict()
+
+    def calculateAllNeighbors(self, neighborRules):
+        """
+        Calculates which territories are connecte to which others based on a ruleset.
+
+        Args:
+            neighborRules (dict): Follows the format:
+                {
+                    "t0": {values for some keys in t0.details},
+                    "t1": {values for some keys in t1.details},
+                    "maxDist": Maximum distance the two territories can be from each other and be connected.
+                }
+        """
+
+        for t0name, t0 in self.territories.items():
+            for t1name, t1 in self.territories.items():
+
+                if t0name == t1name: continue
+
+                for rule in neighborRules:
+
+                    for key, value in rule["t0"].items():
+                        if (t0.details[key] != value):
+                            continue
+
+                    for key, value in rule["t1"].items():
+                        if (t1.details[key] != value):
+                            continue
+
+                    pointDist = (((t0.pos[0] - t1.pos[0])**2) + ((t0.pos[1] - t1.pos[1])**2))**0.5
+                    if (pointDist <= rule["maxDist"]):
+                        t0.edges[t1name] = pointDist
+                        t1.edges[t0name] = pointDist
+
+                    continue
