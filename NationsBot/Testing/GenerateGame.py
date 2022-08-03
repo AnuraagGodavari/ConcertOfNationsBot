ae050be60c31d745499678b9c8f64504268a37cd
AnuraagGodavari <anuraag.godavari@gmail.com>
2022-08-03 17:41:31 -0400

    Added automatic neighbor calculation for maps

--- NationsBot/Testing/GenerateGame.py
+++ NationsBot/Testing/GenerateGame.py
@@ -7,15 +7,24 @@ from Utils import FileHandling, Mapping
 from ConcertOfNationsEngine.GameHandling import *
 from ConcertOfNationsEngine.GameObjects import *
 
-def generateTestWorld():
+def generateTestWorld(length, height, space):
     logInfo("Generating 'Test World' Worldmap...")
 
     world = Mapping.World("Test World")
 
-    world.territories = {
-        "Test (1,1)": Mapping.Territory((1, 1)),
-        "Test (2,2)": Mapping.Territory((2, 2))
-    }
+    world.territories = { 
+        f"Test ({x},{y})": Mapping.Territory((x, y), details = {"Terrain": "Plains"}) 
+        for x in range(0, length, space) for y in range(0, height, space) }
+
+    world.calculateAllNeighbors(
+        [
+            {
+                "t0": {"Terrain": "Plains"},
+                "t1": {"Terrain": "Plains"},
+                "maxDist": 1
+            }
+        ]
+    )
 
     with open(f"{worldsDir}/Test World.json", 'w') as f:
         json.dump(FileHandling.saveObject(world), f, indent = 4)
@@ -26,7 +35,7 @@ def generateTestWorld():
 def generateGame():
     logInfo("Beginning game generation!")
     
-    testWorld = generateTestWorld()
+    testWorld = generateTestWorld(5, 5, 1)
 
     savegame = Savegame(
         "TestGame",
