808e9d6807db22b9088976958f9ef0ab9bd6f3aa
AnuraagGodavari <anuraag.godavari@gmail.com>
2022-09-29 20:46:33 -0400

    Added command to initialize a game for a server

--- NationsBot/Cogs/AdminCommands.py
+++ NationsBot/Cogs/AdminCommands.py
@@ -1,4 +1,4 @@
-import json, datetime, pprint, traceback
+import json, datetime, pprint, traceback, re
 
 import discord
 from discord.ext import commands
@@ -28,6 +28,9 @@ class AdminCommands(commands.Cog):
         Add a nation (by role) and player to a savegame.
         """
 
+        logInfo(f"addNation({ctx.guild.id}, {roleid}, {playerid})")
+
+        #Get savegame info from database
         try:
             savegame = FileHandling.loadObject(load_saveGame(dbget_saveGame_byServer(ctx.guild.id)["savefile"]))
         except Exception as e:
@@ -41,6 +44,7 @@ class AdminCommands(commands.Cog):
 
         logInfo(f"Adding nation {role.name} to saveGame")
 
+        #Try adding the nation to the savegame
         try:
             savegame.add_Nation(Nation(role.name, role.id, (role.color.r, role.color.g, role.color.b)))
         except Exception as e:
@@ -48,6 +52,7 @@ class AdminCommands(commands.Cog):
             await ctx.send(f"Could not add {roleid}: {str(e)}")
             return
 
+        #Try adding the nation to the database - including player, role, and playergame tables
         try:
             add_Nation(
                 savegame, 
@@ -64,6 +69,40 @@ class AdminCommands(commands.Cog):
         save_saveGame(savegame)
 
         await ctx.send(f"Added {roleid} to this game!")
+
+    @commands.command()
+    async def initGame(self, ctx, name, date):
+        """
+        Called in a server without an attached game to initialize a game
+        """
+
+        logInfo(f"initGame({ctx.guild.id}, {name}, {date})")
+
+        try: month, year = (int(x) for x in re.split(r'[,/\\]', date))
+        except:
+            await ctx.send("Could not parse the date. Please write in one of the following formats without spaces: \n> monthnumber,yearnumber\n> monthnumber/yearnumber")
+            return
+
+        savegame = Savegame(
+            name,
+            ctx.guild.id,
+            {"m": month, "y": year},
+            1
+        )
+        
+        try:
+            setupNew_saveGame(
+                savegame, 
+                "Test World", 
+                "Test Gamerule"
+                )
+        except Exception as e:
+            logInfo("Savegame already in database")
+            await ctx.send("This server already has a game assigned to it")
+            return
+
+        logInfo(f"Successfully created savegame {savegame.name} for server {ctx.guild.id}")
+        await ctx.send(f"Successfully initialized game \"{savegame.name}\" at date {savegame.date} and turn {savegame.turn} for this server!")
 
         
 def setup(client):
