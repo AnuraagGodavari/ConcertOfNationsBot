4941c93574c78b0bc7d94d1cb9e42896615c5db8
AnuraagGodavari <anuraag.godavari@gmail.com>
2022-11-23 21:11:37 -0500

    Added stackLevel param to logger functions, indicating how far up the stack we should go to get the name of the function we want to log.

--- NationsBot/ConcertOfNationsEngine/CustomExceptions.py
+++ NationsBot/ConcertOfNationsEngine/CustomExceptions.py
@@ -8,16 +8,16 @@ class NonFatalError(CustomException):
     
     def __init__(self, args):
         super().__init__(args)
-        logInfo(str(args[0]))
+        logInfo(str(args[0]), stackLevel = 1)
 
 class InputError(CustomException):
     
     def __init__(self, args):
         super().__init__(args)
-        logInfo(f"Input Error: \"{str(args)}\"")
+        logInfo(f"Input Error: \"{str(args)}\"", stackLevel = 1)
 
 class GameError(CustomException):
     
     def __init__(self, args):
         super().__init__(args)
-        logInfo(f"Game Error: \"{str(args)}\"")
+        logInfo(f"Game Error: \"{str(args)}\"", stackLevel = 1)
