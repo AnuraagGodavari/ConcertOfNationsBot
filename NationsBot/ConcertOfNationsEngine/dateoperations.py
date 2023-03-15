f280bb9bd8dd9dbc5c4ead50e2bc0b08210834ef
AnuraagGodavari <anuraag.godavari@gmail.com>
2023-03-14 21:33:39 -0400

    Added operations for managing territories and dates in their own files

---
+++ NationsBot/ConcertOfNationsEngine/dateoperations.py
@@ -0,0 +1,21 @@
+from math import *
+
+def date_fromstr(datestr):
+
+    dateSplit = datestr.split('/')
+
+    return {'m': int(dateSplit[0]), 'y': int(dateSplit[-1])}
+
+def date_tostr(date):
+    return f"{date['m']}/{date['y']}"
+
+def date_add(date, numMonths):
+    
+    return {
+        'm': min(12, date['m'] + numMonths),
+        'y': floor(((date['y'] * 12) + date['m'] + numMonths) / 12)
+        }
+
+def date_grtrThan(date0, date1):
+    
+    return (date0['m'] + (date0['y']*12)) > (date1['m'] + (date1['y']*12))
