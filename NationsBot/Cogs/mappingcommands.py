34f9a62c1dc1045f44f9a7ee787a5c95b6558b5d
AnuraagGodavari <anuraag.godavari@gmail.com>
2023-04-24 19:54:34 -0400

    Command worldmap: filled out to allow all players to see the world map from their perspective. Tentatively identical to worldmap_full output

--- NationsBot/Cogs/mappingcommands.py
+++ NationsBot/Cogs/mappingcommands.py
@@ -24,30 +24,54 @@ class MappingCommands(commands.Cog):
         self.client = client
         
     @commands.command()
-    async def nationmap(self, ctx):
-        pass
-
-    @commands.command()
     async def worldmap(self, ctx, roleid = None):
-        logInfo("Not accounting for 'known world'")
         """
-        logInfo(f"worldmap({ctx.guild.id})")
+        Look at the world map from the perspective of a country
+        """
+        logInfo(f"worldmap({ctx.guild.id}, {roleid})")
 
         savegame = get_SavegameFromCtx(ctx)
         if not (savegame): 
-            return
+            raise NonFatalError("No savegame attached to this server")
 
         world = savegame.getWorld()
         if not (world):
             raise InputError("Savegame's world could not be retrieved")
 
-        nation = None
+        #Handles getting the world map if one exists that represents the current gamestate, or creating a new one otherwise.
+        savegame.world_toImage(mapScale = (100, 100))
+        worldMapInfo = dbget_worldMap(world, savegame, savegame.turn)
 
-        if (roleid):
-            nation = get_NationFromRole(ctx, roleid, savegame)
-            if not (nation): 
-                raise InputError(f"Could not get nation with role {roleid}")
-        """
+        logInfo("Got a matching world map for this game.", details = {k: v for k, v in worldMapInfo.items() if k != 'created'})
+        
+        #Make a menu for the world map display
+
+        menu = MenuEmbed(
+            f"{savegame.name} World Map", 
+            "_Territories are displayed by their IDs. Use the command \"terr\_lookup <id>\" to see more information about a territory!_", 
+            ctx.author.id,
+            imgurl = worldMapInfo['link'],
+            fields = [
+                (f"Territory {i}", {
+                    "Name": terr.name, 
+                    "Coordinates": {'x': terr.pos[0], 'y': terr.pos[1]},
+                    "Resources": terr.resources
+                    }
+                ) 
+                for i, terr in enumerate(world.territories)
+            ],
+            pagesize = 9,
+            sortable = True,
+            isPaged = True
+            )
+
+        assignMenu(ctx.author.id, menu)
+
+        logInfo(f"Created worldmap_full menu and assigned it to player {ctx.author.id}")
+
+        await ctx.send(embed = menu.toEmbed(), view = menu.embedView())
+
+        save_saveGame(savegame)
         
     @commands.command()
     @commands.has_permissions(administrator = True)
