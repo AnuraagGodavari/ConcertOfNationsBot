a333e55590975c7d9cf3dc9ddd27dc4f5f1e2040
AnuraagGodavari <anuraag.godavari@gmail.com>
2023-06-11 14:10:31 -0400

    Added functionality for nations to build units and add them to their military

--- NationsBot/ConcertOfNationsEngine/military.py
+++ NationsBot/ConcertOfNationsEngine/military.py
@@ -30,3 +30,40 @@ def get_blueprint(unitType, gamerule):
         raise InputError("Could not find unit in gamerule")
 
     return allunits[unitType]
+
+def new_unitName(military_dict, name_template = "Unit", num = 1):
+    """ 
+    Generate a name for a unit, adding the lowest possible number to the unit name_template
+    
+    Args:
+        name_template (str): The base name which the lowest possible number is appended to to create the actual unit name.
+        num (int): The number the function is checking to see if it is available
+    """
+
+    if (f"{name_template} {num}" not in military_dict.keys()):
+        return f"{name_template} {num}"
+
+    return new_unitName(military_dict, name_template, num + 1)
+
+
+    
+
+class Unit:
+    """
+    Represents a division of a military force with a certain size and type.
+
+    Args:
+        name (str): The unique name of this unit
+        status (str): Describes the state of this unit
+        unitType(str): The type of unit this is; the name of this unit's blueprint in the gamerule
+        size (int): The number of soldiers in this unit
+        home (str): The name of the territory from which this unit was recruited
+    """
+
+    def __init__(self, name: str, status:str, unitType: str, size: int, home: str):
+        
+        self.name = name
+        self.status = status
+        self.unitType = unitType
+        self.size = size
+        self.home = home
