52c638bcac4dadc1fb12895ed44487cedcf31ae6
AnuraagGodavari <anuraag.godavari@gmail.com>
2023-11-25 17:38:45 -0500

    Added working schema and validator functions for worldmaps

--- NationsBot/GameUtils/schema.py
+++ NationsBot/GameUtils/schema.py
@@ -20,27 +20,53 @@ class SchemaProperties:
         self.primitive_type = primitive_type
         self.is_required = is_required
 
-    def validate(self, input_obj, path):
+    def validate(self, input_obj, path, **kwargs):
+        """
+        Validate a value in a json object defined by this SchemaProperties instance.
+
+        Args:
+            input_obj: The object to validate
+            path: Where in the total schema we are looking
+            **kwargs: Optional keyword args that may be used by a dedicated validator function.
+        """
 
         logInfo(f"Validating Schema Path: {path}")
         
         if self.validator:
-            self.validator(input_obj, path)
+            self.validator(input_obj, path, **kwargs)
         
         elif self.primitive_type:
             
             if type(input_obj).__name__!= self.primitive_type:
                 raise InputError(f"{path}: Must be type: {self.primitive_type}")
-                
 
-def schema_validate_list(schema, input_obj, path):
+
+def schema_validate_list(schema, input_obj, path, **kwargs):
+    """
+    Validate a schema object of a list type
+
+    Args:
+        schema: The structure to compare against
+        input_obj: The object to validate
+        path: Where in the total schema we are looking
+        **kwargs: Optional keyword args that may be used by a dedicated validator function.
+    """
 
     logInfo(f"Validating Schema Path: {path}")
 
     for i, element in enumerate(input_obj):
-        schema_validate(schema[0], element, path + f'[{i}]')
+        schema_validate(schema[0], element, path + f'[{i}]', **kwargs)
 
-def schema_validate_dict(schema, input_obj, path):
+def schema_validate_dict(schema, input_obj, path, **kwargs):
+    """
+    Validate a schema dict/js object
+
+    Args:
+        schema: The structure to compare against
+        input_obj: The object to validate
+        path: Where in the total schema we are looking
+        **kwargs: Optional keyword args that may be used by a dedicated validator function.
+    """
 
     logInfo(f"Validating Schema Path: {path}")
 
@@ -53,23 +79,34 @@ def schema_validate_dict(schema, input_obj, path):
             if (val.is_required and key not in input_obj.keys()):
                 raise InputError(f"{path}: Required key {key} not present in object")
 
-            val.validate(input_obj[key], currpath)
+            val.validate(input_obj[key], currpath, **kwargs)
 
         else:
-            schema_validate(val, input_obj[key], currpath)
-        
+            schema_validate(val, input_obj[key], currpath, **kwargs)
 
-def schema_validate(schema, input_obj, path = 'schema'):
+def schema_validate(schema, input_obj, path = 'schema', **kwargs):
+    """
+    Validate a schema object of any valid type
+
+    Args:
+        schema: The structure to compare against
+        input_obj: The object to validate
+        path: Where in the total schema we are looking
+        **kwargs: Optional keyword args that may be used by a dedicated validator function.
+    """
 
     logInfo(f"Validating Schema Path: {path}")
 
     if isinstance(schema, SchemaProperties):
-        schema.validate(input_obj, path)
+        schema.validate(input_obj, path, **kwargs)
 
     elif isinstance(schema, list):
-        schema_validate_list(schema, input_obj, path)
+        schema_validate_list(schema, input_obj, path, **kwargs)
 
     elif isinstance(schema, dict):
-        schema_validate_dict(schema, input_obj, path)
+        schema_validate_dict(schema, input_obj, path, **kwargs)
+
+    else:
+        raise InputError(f"{path}: A schema object's value must either be a dict/json object, a list or a SchemaProperties instance.")
         
 
--- NationsBot/Schemas/schema_worldmap.py
+++ NationsBot/Schemas/schema_worldmap.py
@@ -21,7 +21,10 @@ import ConcertOfNationsEngine.populations as populations
 import ConcertOfNationsEngine.military as military
 import ConcertOfNationsEngine.diplomacy as diplomacy
 
-def validate_territory_pos(pos, path):
+
+# Territory validations
+
+def validate_territory_pos(pos, path, **kwargs):
     """
     Territory positions must be a list of two numbers, like so: [x,y]
     """
@@ -38,14 +41,59 @@ def validate_territory_pos(pos, path):
         if not(isinstance(coord, (int, float, complex))):
             raise InputError(f"{path}: Coordinates must be numbers")
 
-def validate_territory_edges(edges, path):
-    pass
+def validate_territory_edges(edges, path, worldmap = None, **kwargs):
+    """
+    Territory edges must be an object where the keys are territory ids and the values are distances to those territories.
+    """
 
-def validate_territory_details(input_json, path):
-    pass
+    if not (isinstance(edges, dict)):
+        raise InputError(f"{path}: A territory's edges must be in a dictionary/json object.")
 
-def validate_territory_resources(input_json, path):
-    pass
+    all_territories = [territory["id"] for territory in worldmap["territories"]]
+
+    for neighbor, dist in edges.items():
+
+        if (int(neighbor) not in all_territories):
+            raise InputError(f"{path}: Key {neighbor} in edges must be a string of a number equal to another territory's id.")
+
+        if not (isinstance(dist, (int, float, complex))):
+            raise InputError(f"{path}: Edges must be numbers")
+
+def validate_territory_resources(resources, path, gamerule = None, **kwargs):
+    """
+    Territory resources must be an object where the keys are resources that exist in the gamerule and the values are numbers.
+    """
+
+    if not (gamerule):
+        raise InputError(f"{path}: There must be a gamerule provided in order to validate territory resources.")
+
+    if not (isinstance(resources, dict)):
+        raise InputError(f"{path}: A territory's resources must be in a dictionary/json object.")
+
+    for resource, amount in resources.items():
+
+        if (resource not in gamerule["Resources"]):
+            raise InputError(f"{path}: Key {resource} in resources must be a resource which exists in the gamerule.")
+
+        if not (isinstance(amount, (int, float, complex))):
+            raise InputError(f"{path}: Resource amounts must be numbers")
+
+
+# Territory details validations
+
+def validate_terrain(terrain, path, gamerule = None, **kwargs):
+    """
+    Territory details terrain must be a string in Gamerule["Terrain Data"].
+    """
+
+    if not (gamerule):
+        raise InputError(f"{path}: There must be a gamerule provided in order to validate territory resources.")
+
+    if not (isinstance(terrain, str)):
+        raise InputError(f"{path}: A territory's terrain must be a string.")
+
+    if (terrain not in gamerule["Terrain Data"]):
+        raise InputError(f"{path}: Key {terrain} in terrain must be a terrain type which exists in the gamerule.")
 
 
 schema_worldmap = {
@@ -57,7 +105,9 @@ schema_worldmap = {
             "id": schema.SchemaProperties(primitive_type = int.__name__),
             "pos": schema.SchemaProperties(validator = validate_territory_pos),
             "edges": schema.SchemaProperties(validator = validate_territory_edges),
-            "details": schema.SchemaProperties(validator = validate_territory_details),
+            "details": {
+                "Terrain": schema.SchemaProperties(validator = validate_terrain)
+            },
             "resources": schema.SchemaProperties(validator = validate_territory_resources)
         }
     ]
