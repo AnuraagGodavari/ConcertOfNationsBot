2595300497e63856d15735e1aaa8cc8990aff30f
AnuraagGodavari <anuraag.godavari@gmail.com>
2025-01-26 17:36:16 -0500

    File that updates WorldMaps to GameWorldMaps now creates the new WorldMaps table as well as of that version.

--- Database/WorldMaps_to_GameWorldMaps.sql
+++ Database/WorldMaps_to_GameWorldMaps.sql
@@ -1,3 +1,15 @@
 USE ConcertOfNations;
 
 ALTER TABLE `WorldMaps` RENAME `GameWorldMaps`;
+
+CREATE TABLE IF NOT EXISTS `WorldMaps` (
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `world_id` BIGINT UNSIGNED NOT NULL,
+    `version` INT UNSIGNED NOT NULL,
+    `filename` VARCHAR(128),
+    `link` VARCHAR(128) UNIQUE,
+    `created` timestamp NOT NULL DEFAULT current_timestamp(),
+    PRIMARY KEY (`id`),
+    CONSTRAINT `WorldMaps_ibfk_1` FOREIGN KEY (`world_id`) REFERENCES `Worlds` (`id`),
+    UNIQUE(`world_id`, `version`)
+);
